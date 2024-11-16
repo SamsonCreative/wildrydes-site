298f0ef5ad2cf70aef9ca03c14e30836f605dd4a
SamsonCreative <samsondalmeida@gmail.com>
2024-11-16 18:18:38 +0000

    Added config userPool

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+  cognito: {
+    userPoolId: 'eu-west-2_GWkiwYthV', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '3gg8igmak29v8q2c6hikf598jt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-west-2', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+}
